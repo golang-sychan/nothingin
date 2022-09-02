1e69869bab89922713fd2a60a8597c3f2db942f0
ruizhu <ruizhu@alauda.io>
2022-09-02 17:19:30 +0800

    feat Run function

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 func main() {
-  println("hello joey!")
+	println("hello joey!")
 }
 
-func demo(){
-  fmt.Println("demo")
+func demo() {
+	fmt.Println("demo")
+}
+
+//add sth
+func Run() error {
+	return nil
 }
