e5eee87c414df21e8b6a29b78d1a49d72fdd8ae0
ruizhu <ruizhu@alauda.io>
2022-09-29 17:27:52 +0800

    feat: change base image

--- main.go
+++ main.go
@@ -5,28 +5,6 @@ import (
 	"time"
 )
 
-/*
- add 8
-add 20
- add 19
- add 17
- ad doadd 18
-add 16
- add 15
- add 14
-oadd 12
- add 10
- add 13
- add 9
- add 7
- add 1
- add 6
- add 5
- add 4
- add  2
- add 3
-*/
-
 func main() {
 	for {
 		fmt.Printf("time:+%v\n", time.Now().Format(time.RFC822))
@@ -37,20 +15,4 @@ func main() {
 
 func demo() {
 	fmt.Println("demo")
-}
-
-func Run() error {
-	fmt.Printf("App run ...\n")
-	return nil
-}
-
-// wrap error for common
-func wrap_err() []error {
-	return nil
-}
-
-type data struct {
-	Name string
-	Data []byte
-	Addr string `json:"addr"`
 }
